c22e1bfa09a4b09cf348eb05c8213714a2078ff1
imanuel122 <imanuelhulu01@gmail.com>
2025-06-26 22:31:48 +0700

    Menambahkan paragraf

--- index.php
+++ index.php
@@ -1,11 +1,19 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
+
+<head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Coba git remote</title>
-  </head>
-  <body>
+</head>
+
+<body>
     <h1>Hello World</h1>
-  </body>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ex voluptatibus reprehenderit enim maxime officiis
+        cumque corporis magnam voluptates facere aliquid. Esse consectetur sed sunt tenetur quo odio vel commodi ex?</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil blanditiis voluptatem eveniet. Pariatur eius
+        laudantium corrupti eos ipsum esse dicta totam, consequuntur soluta explicabo beatae, eligendi maxime nemo
+        itaque molestias?</p>
+</body>
+
 </html>

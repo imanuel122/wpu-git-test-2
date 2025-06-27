ee6db01dfb9cd84ae6d70b9d7cd99f6cd6c6ae47
imanuel122 <imanuelhulu01@gmail.com>
2025-06-27 12:03:13 +0700

    menghapus paragraf

--- index.php
+++ index.php
@@ -9,11 +9,6 @@
 
 <body>
     <h1>Hello World</h1>
-    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ex voluptatibus reprehenderit enim maxime officiis
-        cumque corporis magnam voluptates facere aliquid. Esse consectetur sed sunt tenetur quo odio vel commodi ex?</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil blanditiis voluptatem eveniet. Pariatur eius
-        laudantium corrupti eos ipsum esse dicta totam, consequuntur soluta explicabo beatae, eligendi maxime nemo
-        itaque molestias?</p>
     <ul>
         <li>satu</li>
         <li>dua</li>
